5ab80bc82ea0bf84e3ce5e7f1de8f4d9c53303f5
Aparajita Fishman <aparajita@aparajita.com>
2013-11-25 09:46:44 +0000

    Moved the quotes outside <near>, thus no need to modify that value

--- phplint.py
+++ phplint.py
@@ -10,8 +10,6 @@
 
 """This module provides the PHPLint plugin class."""
 
-import re
-
 from SublimeLinter.lint import Linter
 
 
@@ -22,15 +20,14 @@ class PHPLint(Linter):
     language = ('php', 'html')
     cmd = 'phplint --print-path shortest --print-context --tab-size 4 --no-overall'
     regex = (
-        r'^(?:'
+        r'^(?i)(?:'
         r'\t.*?\r?\n'
         r'\t(?P<col>.*)\\_ HERE\r?\n)?'
         r'(?P<filename>.+?):(?P<line>\d+): '
         r'(?:(?P<error>(?:fatal )?error)|(?P<warning>warning|notice)): '
-        r'(?P<message>[^`\r\n]*(?P<near>`.+\')?[^\r\n]*)$'
+        r'(?P<message>[^`\r\n]*(?:`(?P<near>[^\']+)\')?[^\r\n]*)$'
     )
     multiline = True
-    re_flags = re.IGNORECASE
     tempfile_suffix = 'php'
     selectors = {
         'html': 'source.php.embedded.block.html'
@@ -41,8 +38,5 @@ class PHPLint(Linter):
         match, line, col, error, warning, message, near = super().split_match(match)
 
         message = message.replace('`', '\'')
-
-        if near:
-            near = near.replace('`', '\'')
 
         return match, line, col, error, warning, message, near
