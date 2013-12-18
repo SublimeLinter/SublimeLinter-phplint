ab66e8563a7fee9e43d7f62306861a025ac35bec
Aparajita Fishman <aparajita@aparajita.com>
2013-12-17 22:02:52 -0800

    Fixes and enhancements...
    - Added 'html 5' as a syntax.
    - Adjusted col index, it seemed to be off by one.
    - Removed trailing $ in regex, that broke it in Windows.
    - No embedded linting, php by definition can be an entire html file.
    - Ignore 'no PHP code found at all' error.
    - Strip function references so near can find them.

--- linter.py
+++ linter.py
@@ -17,26 +17,32 @@ class PHPLint(Linter):
 
     """Provides an interface to the phplint executable."""
 
-    syntax = ('php', 'html')
+    syntax = ('php', 'html', 'html 5')
     cmd = 'phplint --print-path shortest --print-context --tab-size 4 --no-overall'
     regex = (
-        r'^(?i)(?:'
+        r'(?i)^(?:'
         r'\t.*?\r?\n'
-        r'\t(?P<col>.*)\\_ HERE\r?\n)?'
+        r'\t(?P<col>.*) \\_ HERE\r?\n)?'
         r'(?P<filename>.+?):(?P<line>\d+): '
         r'(?:(?P<error>(?:fatal )?error)|(?P<warning>warning|notice)): '
-        r'(?P<message>[^`\r\n]*(?:`(?P<near>[^\']+)\')?[^\r\n]*)$'
+        r'(?P<message>[^`\r\n]*(?:`(?P<near>[^\']+)\')?[^\r\n]*)'
     )
     multiline = True
     tempfile_suffix = 'php'
-    selectors = {
-        'html': 'source.php.embedded.block.html'
-    }
 
     def split_match(self, match):
         """Return the match with ` quotes transformed to '."""
         match, line, col, error, warning, message, near = super().split_match(match)
 
-        message = message.replace('`', '\'')
+        if message == 'no PHP code found at all':
+            match = None
+        else:
+            message = message.replace('`', '\'')
+
+            # If the message contains a complaint about a function
+            # and near looks like a function reference, remove the trailing
+            # () so it can be found.
+            if 'function \'' in message and near and near.endswith('()'):
+                near = near[:-2]
 
         return match, line, col, error, warning, message, near
