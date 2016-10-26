fe05d0e0b73a053278cec4295a3db9f38c01cca9
josephmtaylor <joseph.taylor@dodatllc.com>
2016-10-26 11:49:31 -0500

    Setting for php version changing.

--- linter.py
+++ linter.py
@@ -18,7 +18,13 @@ class PHPLint(Linter):
     """Provides an interface to the phplint executable."""
 
     syntax = ('php', 'html')
-    cmd = 'phpl --php-version 5 --print-path relative --print-column-number --tab-size 4 --no-overall'
+
+    """Get settings for PHPLinter"""
+    view_settings = self.get_view_settings(inline=True)
+    php_version = \
+        view_settings.get('php_version', 5)
+
+    cmd = 'phpl --php-version ' + php_version + ' --print-path relative --print-column-number --tab-size 4 --no-overall'
     version_args = '--version'
     version_re = r'PHPLint (?P<version>\d+\.\d+)'
     version_requirement = '>= 2.0'
